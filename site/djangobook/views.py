6da0753fc3427c0c248bb8c8569e2c988fd180ad
jacob <jacob@ab0fe8bf-1409-0410-8df2-a7a41c477d0f>
2007-12-12 23:54:12 +0000

    Make media_base actually work, and comment out the non-working private chapter stuff.
    git-svn-id: http://djangobook.com/svn/trunk@528 ab0fe8bf-1409-0410-8df2-a7a41c477d0f

--- site/djangobook/views.py
+++ site/djangobook/views.py
@@ -4,10 +4,8 @@ import datetime
 from django.conf import settings
 from django.utils import simplejson
 from django.core.cache import cache
-from docutils.core import publish_parts
 from django.template import RequestContext
 from django.http import Http404, HttpResponse
-from django.utils.safestring import mark_safe
 from django.views.decorators.http import require_POST
 from django.contrib.auth.decorators import permission_required, login_required
 from django.shortcuts import render_to_response, get_object_or_404
@@ -42,40 +40,48 @@ def _get_release_or_404(lang, version, type, chapter, show_future_chapters=False
 
 def chapter(request, lang, version, type, chapter):
     release, content = _get_release_or_404(lang, version, type, chapter, request.user.has_perm("djangobook.change_chapter"))
+    
     parts = cache.get("djangobook:rendered_content:%s" % release.id)
     if parts is None or settings.DEBUG or request.GET.has_key("clear_cache"):
-        parts = publish_html(content, media_base="%s/%s/%s/" % (settings.CONTENT_MEDIA_BASE, release.version.language, release.version.version))
+        media_base = settings.CONTENT_MEDIA_BASE + "%s/%s/%s%s/" % (
+            release.version.language,
+            release.version.version,
+            release.get_type_display(), 
+            release.get_number_url_fragment(),
+        )
+        parts = publish_html(content, media_base=media_base)
         cache.set("djangobook:rendered_content:%s" % release.id, parts, 5*60)
+    
     return render_to_response(
-        ["book/%s%s.html" % (release.get_type_display(), release.get_number_display()), "book/chapter.html"], 
+        ["book/%s%s.html" % (release.get_type_display(), release.get_number_url_fragment()), "book/chapter.html"], 
         {"release" : release, "content" : parts},
         RequestContext(request, {})
     )
 
-@login_required
-def private_toc(request, slug):
-    vers = get_object_or_404(PrivateVersion, slug=slug)
-    chapters = list(Chapter.objects.all())
-    for c in chapters:
-        c.exists_in_branch = bool(vers.get_content("%s%si" % (c.get_type_display().lower(), c.get_number_display())))
-    return render_to_response(
-        "book/private_toc.html",
-        {"contents" : chapters, "version" : vers},
-        RequestContext(request, {})
-    )
-
-@login_required
-def private_chapter(request, slug, type, chapter):
-    vers = get_object_or_404(PrivateVersion, slug=slug)
-    content = vers.get_content(type+chapter)
-    if not content:
-        raise Http404
-    parts = publish_parts(source=content, writer=DjangoBookHTMLWriter(), settings_overrides={'initial_header_level' : 3})
-    return render_to_response(
-        ["book/private_%s%s.html" % (type, chapter), "book/private_chapter.html"], 
-        {"version" : vers, "content" : parts},
-        RequestContext(request, {})
-    )
+# @login_required
+# def private_toc(request, slug):
+#     vers = get_object_or_404(PrivateVersion, slug=slug)
+#     chapters = list(Chapter.objects.all())
+#     for c in chapters:
+#         c.exists_in_branch = bool(vers.get_content("%s%si" % (c.get_type_display().lower(), c.get_number_display())))
+#     return render_to_response(
+#         "book/private_toc.html",
+#         {"contents" : chapters, "version" : vers},
+#         RequestContext(request, {})
+#     )
+# 
+# @login_required
+# def private_chapter(request, slug, type, chapter):
+#     vers = get_object_or_404(PrivateVersion, slug=slug)
+#     content = vers.get_content(type+chapter)
+#     if not content:
+#         raise Http404
+#     parts = publish_parts(source=content, writer=DjangoBookHTMLWriter(), settings_overrides={'initial_header_level' : 3})
+#     return render_to_response(
+#         ["book/private_%s%s.html" % (type, chapter), "book/private_chapter.html"], 
+#         {"version" : vers, "content" : parts},
+#         RequestContext(request, {})
+#     )
 
 def comments(request, lang, version, type, chapter, nodenum=None):
     release, content = _get_release_or_404(lang, version, type, chapter)
