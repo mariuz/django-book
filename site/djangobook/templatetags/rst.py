60ea6e56e967eae1ca677b2e8440d6f74b3ac797
jacob <jacob@ab0fe8bf-1409-0410-8df2-a7a41c477d0f>
2007-12-14 16:51:39 +0000

    Mark rst as safe.
    git-svn-id: http://djangobook.com/svn/trunk@547 ab0fe8bf-1409-0410-8df2-a7a41c477d0f

--- site/djangobook/templatetags/rst.py
+++ site/djangobook/templatetags/rst.py
@@ -1,8 +1,9 @@
 from django import template
 from djangobook.rst import publish_html
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 @register.filter
 def rst(text):
-    return publish_html(text)['body']
+    return mark_save(publish_html(text)['body'])
